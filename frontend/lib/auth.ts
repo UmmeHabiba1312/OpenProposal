c698f54142c7635d35006791ad780c6ad6932622
UmmeHabiba1312 <ummeyhabiba1312@gmail.com>
2026-09-05 15:58:23 +0500

    change url

--- frontend/lib/auth.ts
+++ frontend/lib/auth.ts
@@ -3,7 +3,7 @@ import { Pool } from "pg";
 import { jwt } from "better-auth/plugins";
 
 export const auth = betterAuth({
-  baseURL: process.env.BETTER_AUTH_URL || "https://open-proposal-nine.vercel.app/",
+  baseURL: process.env.BETTER_AUTH_URL || "https://open-proposal-nine.vercel.app",
   secret: process.env.BETTER_AUTH_SECRET,
   database: new Pool({ connectionString: process.env.DATABASE_URL }),
   emailAndPassword: {
